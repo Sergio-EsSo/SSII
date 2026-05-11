c7592f71b1ce107b249677c37aa1c9d37c26c286
Sergio-EsSo <seesso2003@gmail.com>
2026-05-11 12:51:46 +0200

    11/5/26

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,11 +4,17 @@ async function cargarUsuarios(){
     // Obtener loos usuarios en forma json
     const response = await fetch("http://localhost:8080/users") // GET  1s
     const usuarios = await response.json()
+    const contenedor = document.getElementById("usuarios")
+    contenedor.innerHTML=""
     usuarios.forEach(user  => {
         // Creo un div nuevo
         const card = document.createElement("div");
+        card.className="usuario_card"
         // Meto dentro del div con id = usuarios
-
+        contenedor.appendChild(card)
+        card.innerHTML = `
+            <div>${user.name}</div>
+            <div>${user.email}</div>
+        `
     })
-
 }
